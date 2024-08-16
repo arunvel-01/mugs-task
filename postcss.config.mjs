46df7c5ed9813b008c9e54f4577ea3abb3f00fd5
Arun Vel <arunvelv9253@gmail.com>
2024-08-16 23:45:06 +0530

    Updated Readme

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
-module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'autoprefixer',
-  ],
-}
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
